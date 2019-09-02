85455f0f6fca3ba821a5b5401cf1e142d8a5029e
Paul <paulbyrnew@gmail.com>
2019-09-02 18:55:36 +0100

    added allergies

--- app/controllers/medical_records_controller.rb
+++ app/controllers/medical_records_controller.rb
@@ -6,6 +6,8 @@ class MedicalRecordsController < ApplicationController
 
     @patient = Patient.find_by(params[session[:patient_id]])
     @medical_records = @patient.medical_records
+    @medical_records = @patient.medical_records.search(search)
+    # @medical_records = @patient.medical_records.search(search)
     # search = params[:search]
     # # category = params[:category]
     # @medical_records = MedicalRecord.all.search(search)
